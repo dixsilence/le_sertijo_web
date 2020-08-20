ea5cc4bb887b4739353cb5b8f7466f8eb3648d0c
rodrigo_calderon <dixsilence@gmail.com>
2020-08-20 19:29:09 -0400

    Casi completo

---
+++ assets/jquery/carousel.js
@@ -0,0 +1,21 @@
+$('#carrusel-taller').carousel({
+    interval: 8000
+})
+
+$('.carousel  .test').each(function() {
+    var minPerSlide = 4;
+    var next = $(this).next();
+    if (!next.length) {
+        next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+
+    for (var i = 0; i < minPerSlide; i++) {
+        next = next.next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+
+        next.children(':first-child').clone().appendTo($(this));
+    }
+});
